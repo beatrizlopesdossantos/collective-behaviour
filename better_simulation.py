7ff4f7c56b1c69940d4736ee3d453df6f0c82951
Tomas Marek <485386@mail.muni.cz>
2023-12-10 13:36:18 +0100

    vision radius view fixed

--- better_simulation.py
+++ better_simulation.py
@@ -7,14 +7,14 @@ import math
 pygame.init()
 
 # Constants
-WIDTH, HEIGHT = 1200, 800
+WIDTH, HEIGHT = 1500, 800
 BG_COLOR = (255, 255, 255)
 BIRD_COLOR = (0, 0, 0)
 VISION_COLOR = (200, 200, 200)
 NUM_BIRDS = 50
 VISION_ANGLE = 100
-VISION_RADIUS = 50
-TURN_SPEED = 0.1
+VISION_RADIUS = 30
+TURN_SPEED = 0.3
 SPEED = 2
 
 # Bird class
@@ -23,25 +23,26 @@ class Bird:
         self.x = x
         self.y = y
         self.angle = random.uniform(0, 2 * math.pi)
+        self.group_count = 0
 
     def update(self, birds):
         # Check for nearby birds in vision
         nearby_birds = [b for b in birds if b != self and self.in_vision(b)]
+        if len(nearby_birds) == 1:
+            self.group_count = 1
+            nearby_birds[0].group_count = 1
 
         if len(nearby_birds) != 0:
 
-            avg_position = (
-                sum([bird.x for bird in nearby_birds]) / len(nearby_birds),
-                sum([bird.y for bird in nearby_birds]) / len(nearby_birds),
-            )
-
-            vector_x = avg_position[0] - self.x
-            vector_y = avg_position[1] - self.y
-            direction_radians = math.atan2(vector_y, vector_x)
-            self.angle = (direction_radians + 2 * math.pi) % (2 * math.pi)
+            avg_angle = sum([b.angle for b in nearby_birds]) / len(nearby_birds)
+            self.angle = (avg_angle + 2 * math.pi) % (2 * math.pi)
+            self.neighbor_count = len(nearby_birds)
         
         else:
 
+            self.angle += random.uniform(-TURN_SPEED, TURN_SPEED)
+
+        if self.group_count == 0:
             self.angle += random.uniform(-TURN_SPEED, TURN_SPEED)
 
         # Move forward
@@ -49,13 +50,9 @@ class Bird:
         self.y = (self.y + SPEED * math.sin(self.angle)) % HEIGHT
 
     def in_vision(self, other_bird):
-        angle_to_other = math.atan2(other_bird.y - self.y, other_bird.x - self.x)
-        angle_diff = abs(angle_to_other - self.angle)
+        # Check if the other bird is within vision radius
+        return math.hypot(other_bird.x - self.x, other_bird.y - self.y) < VISION_RADIUS
 
-        # Check if the other bird is within vision angle and radius
-        return angle_diff < math.radians(VISION_ANGLE / 2) and math.hypot(
-            other_bird.x - self.x, other_bird.y - self.y
-        ) < VISION_RADIUS
 
 # Create birds
 birds = [Bird(random.randint(0, WIDTH), random.randint(0, HEIGHT)) for _ in range(NUM_BIRDS)]
@@ -86,12 +83,15 @@ while True:
         vision_end_x = bird.x + VISION_RADIUS * math.cos(bird.angle - 0.75 * math.pi)
         vision_end_y = bird.y + VISION_RADIUS * math.sin(bird.angle - 0.75 * math.pi)
         pygame.draw.line(screen, VISION_COLOR, (bird.x, bird.y), (vision_end_x, vision_end_y))
+        tmp = -1 * bird.angle
         pygame.draw.arc(screen, 
                         VISION_COLOR, 
                         pygame.Rect(bird.x - VISION_RADIUS, bird.y - VISION_RADIUS, VISION_RADIUS*2, VISION_RADIUS*2), 
-                        bird.angle - 0.75 * math.pi, 
-                        bird.angle + 0.75 * math.pi,
+                        (tmp - 0.75 * math.pi + math.pi * 2 ) % (2 * math.pi),
+                        (tmp + 0.75 * math.pi + math.pi * 2 ) % (2 * math.pi), 
                         1)
+
+
 
     pygame.display.flip()
     pygame.time.Clock().tick(30)
